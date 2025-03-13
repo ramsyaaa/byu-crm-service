71fefd187f7c0ff1093cd6002f3a09b99811aa72
alifnrz27 <alifnaufalrizki27@gmail.com>
2025-03-13 16:26:41 +0700

    edit import account

--- modules/account/service/service_impl.go
+++ modules/account/service/service_impl.go
@@ -6,6 +6,7 @@ import (
 	cityRepository "byu-crm-service/modules/city/repository"
 	"errors"
 	"fmt"
+	"strconv"
 )
 
 type accountService struct {
@@ -40,14 +41,66 @@ func (s *accountService) GetAllAccounts(limit, page int, search, userRole, terri
 	return accounts, pagination, nil
 }
 
+// func (s *accountService) ProcessAccount(data []string) error {
+// 	existingAccount, err := s.repo.FindByAccountCode(data[8]) // AccountCode
+// 	if err != nil {
+// 		return err
+// 	}
+// 	if existingAccount != nil {
+// 		return nil
+// 	}
+
+// 	// Assuming you have a cityService with a method FindCityByName
+// 	city, err := s.cityRepo.FindByName(data[4]) // City
+// 	if err != nil {
+// 		return err
+// 	}
+// 	if city == nil {
+// 		return fmt.Errorf("city not found")
+// 	}
+
+// 	account := models.Account{
+// 		AccountName:             &data[5],
+// 		AccountType:             &data[6],
+// 		AccountCategory:         &data[7],
+// 		AccountCode:             &data[8],
+// 		City:                    &city.ID, // Set city ID
+// 		ContactName:             &data[9],
+// 		EmailAccount:            &data[10],
+// 		WebsiteAccount:          &data[12],
+// 		Potensi:                 &data[11],
+// 		SystemInformasiAkademik: &data[13],
+// 		Ownership:               &data[14],
+// 	}
+// 	return s.repo.Create(&account)
+// }
+
 func (s *accountService) ProcessAccount(data []string) error {
-	existingAccount, err := s.repo.FindByAccountCode(data[8]) // AccountCode
-	if err != nil {
-		return err
-	}
-	if existingAccount != nil {
+	if isZeroValue(data[14]) || isZeroValue(data[15]) {
+		fmt.Println("data not found")
 		return nil
+	} else {
+		fmt.Println("data found")
+
+		existingAccount, err := s.repo.FindByAccountCode(data[0]) // AccountCode
+		if err != nil {
+			return err
+		}
+
+		if existingAccount == nil {
+			fmt.Println("account not found")
+			return nil
+		}
+
+		updateData := map[string]interface{}{
+			"longitude": data[15],
+			"latitude":  data[14],
+		}
+
+		return s.repo.UpdateFields(existingAccount.ID, updateData)
 	}
+
+	return nil
 
 	// Assuming you have a cityService with a method FindCityByName
 	city, err := s.cityRepo.FindByName(data[4]) // City
@@ -72,4 +125,12 @@ func (s *accountService) ProcessAccount(data []string) error {
 		Ownership:               &data[14],
 	}
 	return s.repo.Create(&account)
+}
+
+func isZeroValue(value string) bool {
+	parsed, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		return false // Jika tidak bisa diubah ke float, berarti bukan angka nol
+	}
+	return parsed == 0
 }
