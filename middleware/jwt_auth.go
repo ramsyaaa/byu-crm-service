708a02b5aabf3bbe4eee77639dd3b9135eab32a2
alifnrz27 <alifnaufalrizki27@gmail.com>
2025-04-07 09:05:40 +0700

    edit middleware

--- middleware/jwt_auth.go
+++ middleware/jwt_auth.go
@@ -2,7 +2,6 @@ package middleware
 
 import (
 	"byu-crm-service/helper"
-	"fmt"
 	"os"
 
 	jwtware "github.com/gofiber/contrib/jwt"
@@ -11,29 +10,12 @@ import (
 )
 
 func JWTMiddleware(c *fiber.Ctx) error {
-	authHeader := c.Get("Authorization")
-
-	if authHeader == "" {
-		return jwtErrorHandler(c, fiber.ErrUnauthorized)
-	}
-
-	fmt.Println(authHeader)
-
-	const bearerPrefix = "Bearer "
-	if len(authHeader) <= len(bearerPrefix) || authHeader[:len(bearerPrefix)] != bearerPrefix {
-		return jwtErrorHandler(c, fiber.NewError(fiber.StatusUnauthorized, "Token harus diawali dengan 'Bearer '"))
-	}
-
-	// Ambil token tanpa "Bearer "
-	tokenOnly := authHeader[len(bearerPrefix):]
-	c.Request().Header.Set("Authorization", tokenOnly) // ganti header agar jwtware bisa proses
-
 	// Jalankan middleware jwtware
 	return jwtware.New(jwtware.Config{
-		SigningKey:  jwtware.SigningKey{Key: []byte(os.Getenv("JWT_SECRET"))},
-		ContextKey:  "jwt",
-		TokenLookup: "header:Authorization", // tetap sama
-		// AuthScheme dikosongkan agar jwtware tidak cek prefix
+		SigningKey:   jwtware.SigningKey{Key: []byte(os.Getenv("JWT_SECRET"))},
+		ContextKey:   "jwt",
+		TokenLookup:  "header:Authorization", // tetap sama
+		AuthScheme:   "Bearer",
 		ErrorHandler: jwtErrorHandler,
 	})(c)
 }
