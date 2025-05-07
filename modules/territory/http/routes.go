2ece8fca10f45c6450d4c39f30754c030f822fe8
alifnrz27 <alifnaufalrizki27@gmail.com>
2025-05-07 09:57:09 +0700

    [ADD] get all territory

---
+++ modules/territory/http/routes.go
@@ -0,0 +1,15 @@
+package http
+
+import (
+	"byu-crm-service/middleware"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func TerritoryRoutes(router fiber.Router, handler *TerritoryHandler) {
+	authRouter := router.Group("/territories",
+		middleware.JWTMiddleware,
+		middleware.JWTUserContextMiddleware(),
+	)
+	authRouter.Get("/", handler.GetAllTerritories)
+}
