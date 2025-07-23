16342e63a50ff5b6331511a864b5461de6e75eec
alifnrz27 <alifnaufalrizki27@gmail.com>
2025-07-23 10:18:03 +0700

    EDIT broadcast

--- modules/broadcast/validation/broadcast-validation.go
+++ modules/broadcast/validation/broadcast-validation.go
@@ -7,11 +7,13 @@ import (
 )
 
 type ValidateRequest struct {
-	Title       string    `json:"title" validate:"required"`
-	Description string    `json:"description" validate:"required"`
-	Type        string    `json:"type" validate:"required"`
-	UserID      *[]string `json:"user_id"`
-	RoleID      *[]string `json:"role_id"`
+	Title         string    `json:"title" validate:"required"`
+	Description   string    `json:"description" validate:"required"`
+	Type          string    `json:"type" validate:"required"`
+	UserID        *[]string `json:"user_id"`
+	RoleID        *[]string `json:"role_id"`
+	TerritoryType *string   `json:"territory_type"`
+	TerritoryID   *[]string `json:"territory_id"`
 }
 
 var validationMessages = map[string]string{
@@ -29,7 +31,7 @@ func ValidateCreate(req *ValidateRequest) map[string]string {
 func ValidateByUser(req *ValidateRequest) map[string]string {
 	errors := make(map[string]string)
 	if req.UserID == nil || len(*req.UserID) == 0 {
-		errors["user_id"] = "User ID tidak boleh kosong."
+		errors["user_id"] = "User tidak boleh kosong."
 	}
 
 	if len(errors) == 0 {
@@ -41,7 +43,27 @@ func ValidateByUser(req *ValidateRequest) map[string]string {
 func ValidateByRole(req *ValidateRequest) map[string]string {
 	errors := make(map[string]string)
 	if req.RoleID == nil || len(*req.RoleID) == 0 {
-		errors["role_id"] = "Role ID tidak boleh kosong."
+		errors["role_id"] = "Role tidak boleh kosong."
+	}
+
+	if len(errors) == 0 {
+		return nil
+	}
+	return errors
+}
+
+func ValidateByTerritory(req *ValidateRequest) map[string]string {
+	errors := make(map[string]string)
+	if req.TerritoryID == nil || len(*req.TerritoryID) == 0 {
+		errors["territory_id"] = "Territory tidak boleh kosong."
+	}
+
+	if req.TerritoryType == nil || *req.TerritoryType == "" {
+		errors["territory_type"] = "Type territory tidak boleh kosong."
+	}
+
+	if *req.TerritoryType != "ALL" && *req.TerritoryType != "AREA" && *req.TerritoryType != "REGIONAL" && *req.TerritoryType != "BRANCH" && *req.TerritoryType != "CLUSTER" {
+		errors["territory_type"] = "Type territory tidak valid."
 	}
 
 	if len(errors) == 0 {
