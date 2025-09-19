d45fb09c1594a4835c3191198d67836c7bc891d7
alifnrz27 <alifnaufalrizki27@gmail.com>
2025-09-19 11:53:33 +0700

    [ADD] resume user

--- routes/UserRoute.go
+++ routes/UserRoute.go
@@ -6,12 +6,18 @@ import (
 	authRepository "byu-crm-service/modules/auth/repository"
 	authService "byu-crm-service/modules/auth/service"
 	cityRepo "byu-crm-service/modules/city/repository"
+	kpiYaeRangeRepo "byu-crm-service/modules/kpi-yae-range/repository"
+	kpiYaeRangeService "byu-crm-service/modules/kpi-yae-range/service"
 	roleRepo "byu-crm-service/modules/role/repository"
 	roleService "byu-crm-service/modules/role/service"
 	"byu-crm-service/modules/user/http"
 	"byu-crm-service/modules/user/repository"
 	"byu-crm-service/modules/user/service"
 	"byu-crm-service/modules/user/validation"
+	visitHistoryRepo "byu-crm-service/modules/visit-history/repository"
+	visitHistoryService "byu-crm-service/modules/visit-history/service"
+	yaeLeaderboardRepo "byu-crm-service/modules/yae-leaderboard/repository"
+	yaeLeaderboardService "byu-crm-service/modules/yae-leaderboard/service"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/redis/go-redis/v9"
@@ -28,10 +34,16 @@ func UserRouter(router fiber.Router, db *gorm.DB, redisClient any) {
 	authService := authService.NewAuthService(authRepo)
 	accountService := accountService.NewAccountService(accountRepo, cityRepo)
 	roleService := roleService.NewRoleService(roleRepo)
+	kpiYaeRangeRepo := kpiYaeRangeRepo.NewKpiYaeRangeRepository(db)
+	kpiYaeRangeService := kpiYaeRangeService.NewKpiYaeRangeService(kpiYaeRangeRepo)
+	visitHistoryRepo := visitHistoryRepo.NewVisitHistoryRepository(db)
+	visitHistoryService := visitHistoryService.NewVisitHistoryService(visitHistoryRepo)
+	yaeLeaderboardRepo := yaeLeaderboardRepo.NewYaeLeaderboardRepository(db)
+	yaeLeaderboardService := yaeLeaderboardService.NewYaeLeaderboardService(yaeLeaderboardRepo)
 
 	validation.SetUserRepository(userRepo)
 
-	userHandler := http.NewUserHandler(userService, authService, accountService, roleService, redisClient.(*redis.Client))
+	userHandler := http.NewUserHandler(userService, authService, accountService, roleService, kpiYaeRangeService, visitHistoryService, yaeLeaderboardService, redisClient.(*redis.Client))
 
 	http.UserRoutes(router, userHandler)
 
